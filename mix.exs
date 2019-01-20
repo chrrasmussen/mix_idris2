03504b4cb29c01df3452a4aace61567186826a6a
Christian Rasmussen <christian.rasmussen@me.com>
2019-01-20 03:08:27 +0100

    Cleaned up project

--- mix.exs
+++ mix.exs
@@ -5,16 +5,8 @@ defmodule MixBlodwen.MixProject do
     [
       app: :mix_blodwen,
       version: "0.1.0",
-      elixir: "~> 1.8",
-      start_permanent: Mix.env() == :prod,
+      elixir: "~> 1.5",
       deps: deps()
-    ]
-  end
-
-  # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
-      extra_applications: [:logger]
     ]
   end
 
--- test/mix_blodwen_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule MixBlodwenTest do
-  use ExUnit.Case
-  doctest MixBlodwen
-
-  test "greets the world" do
-    assert MixBlodwen.hello() == :world
-  end
-end
