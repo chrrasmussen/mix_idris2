5043687bb88ca635ae6c2b03bd7bb1f442b27bd1
Christian Rasmussen <christian.rasmussen@me.com>
2020-09-26 18:57:33 +0200

    Add package description

--- mix.exs
+++ mix.exs
@@ -6,11 +6,26 @@ defmodule MixIdris.MixProject do
       app: :mix_idris2,
       version: "0.1.0",
       elixir: "~> 1.5",
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
 
   defp deps do
     []
+  end
+
+  defp description() do
+    "Mix compiler for Idris 2"
+  end
+
+  defp package() do
+    [
+      files: ~w(lib .formatter.exs mix.exs README.md LICENSE),
+      exclude_patterns: [".DS_Store"],
+      licenses: ["BSD-3-Clause"],
+      links: %{"GitHub" => "https://github.com/chrrasmussen/mix_idris2"}
+    ]
   end
 end
